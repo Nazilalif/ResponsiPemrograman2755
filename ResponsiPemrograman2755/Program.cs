d9a2eb9ffa66c7579a93a1a83a516a5f77a68e04
Nazilalif <61883195+Nazilalif@users.noreply.github.com>
2020-04-14 12:06:58 +0700

    Add files via upload

---
+++ ResponsiPemrograman2755/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ResponsiPemrograman2755
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan person = new Karyawan(19112755, 4000000, "Nazil");
+            Karyawan person2 = new Karyawan(19110000, 2000000, "Alifurohman");
+
+            Console.WriteLine("No. \t Nik/Nama \t\t Gaji Bulanan ");
+            Console.WriteLine("1. \t {0} {1} \t {2}", person.Nik, person.Nama, person.GajiBulanan);
+            Console.WriteLine("2. \t {0} {1} \t {2}", person2.Nik, person2.Nama, person2.GajiBulanan);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Asyiiik kenaikan gaji 10%");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("No. \t Nik/Nama \t\t Gaji Bulanan ");
+            Console.WriteLine("1. \t {0} {1} \t {2}", person.Nik, person.Nama, person.UangPlus);
+            Console.WriteLine("2. \t {0} {1} \t {2}", person2.Nik, person2.Nama, person2.UangPlus);
+        }
+    }
+
+    class Karyawan
+    {
+
+        public int Nik { get; set; }
+        public int GajiBulanan { get; set; }
+        public string Nama { get; set; }
+        public int UangPlus { get; set; }
+
+        public Karyawan(int Nik, int Gaji, string Nama)
+        {
+
+            this.Nik = Nik;
+            this.Nama = Nama;
+
+            if (Gaji < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = Gaji;
+            }
+
+            double y = 0;
+            y = 0.1 * GajiBulanan;
+            UangPlus = Convert.ToInt32(y) + GajiBulanan;
+
+        }
+
+    }
+}
